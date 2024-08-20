294423988cf8fb0847bf8183191404dd57e86305
Jerome Otto <jeromeo1969@hotmail.com>
2024-08-20 14:57:51 -0600

    Main Rough Draft

---
+++ index.js
@@ -0,0 +1,11 @@
+//!SECTION Ice Cream and Toppings Array
+
+const iceCream = [
+  { name: 'Chocolate', price: 1.25, quantity: 0, type: 'scoop' },
+  { name: 'Vanilla', price: 1, quantity: 0, type: 'scoop' },
+  { name: 'Strawberry', price: 1.25, quantity: 0, type: 'scoop' },
+  { name: 'Sprinkles', price: 0.25, quantity: 0, type: 'topping' },
+  { name: 'Chocolate Chips', price: 0.25, quantity: 0, type: 'topping' },
+  { name: 'Cookie Chunks', price: 0.50, quantity: 0, type: 'topping' }
+]
+
