18532a7797bd462f908f69062a2d3258321a6e79
Jerome Otto <jeromeo1969@hotmail.com>
2024-08-20 15:13:02 -0600

    Updated iceCream Array with correct pricing

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 //!SECTION Ice Cream and Toppings Array
 
 const iceCream = [
-  { name: 'Chocolate', price: 1.25, quantity: 0, type: 'scoop' },
-  { name: 'Vanilla', price: 1, quantity: 0, type: 'scoop' },
-  { name: 'Strawberry', price: 1.25, quantity: 0, type: 'scoop' },
-  { name: 'Sprinkles', price: 0.25, quantity: 0, type: 'topping' },
-  { name: 'Chocolate Chips', price: 0.25, quantity: 0, type: 'topping' },
-  { name: 'Cookie Chunks', price: 0.50, quantity: 0, type: 'topping' }
+  { name: 'Chocolate', price: 4.00, quantity: 0, type: 'scoop' },
+  { name: 'Vanilla', price: 3.00, quantity: 0, type: 'scoop' },
+  { name: 'Strawberry', price: 4.00, quantity: 0, type: 'scoop' },
+  { name: 'Sprinkles', price: 2.00, quantity: 0, type: 'topping' },
+  { name: 'Chocolate Chips', price: 3.00, quantity: 0, type: 'topping' },
+  { name: 'Gummy Worms', price: 2.00, quantity: 0, type: 'topping' }
+  { name: 'Waffle Cone', price: 3.00, quantity: 0, type: 'cone' }
+  { name: 'Waffle Bowl', price: 4.00, quantity: 0, type: 'cone' }
+  { name: 'Dipped Cone', price: 5.00, quantity: 0, type: 'cone' }
 ]
 
