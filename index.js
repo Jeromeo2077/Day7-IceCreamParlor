3e99ab0468113fc5b37d1c262f15a0a7d83945b1
Jerome Otto <jeromeo1969@hotmail.com>
2024-08-20 15:23:01 -0600

    Fixed iceCream Array syntax

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const iceCream = [
   { name: 'Strawberry', price: 4.00, quantity: 0, type: 'scoop' },
   { name: 'Sprinkles', price: 2.00, quantity: 0, type: 'topping' },
   { name: 'Chocolate Chips', price: 3.00, quantity: 0, type: 'topping' },
-  { name: 'Gummy Worms', price: 2.00, quantity: 0, type: 'topping' }
-  { name: 'Waffle Cone', price: 3.00, quantity: 0, type: 'cone' }
-  { name: 'Waffle Bowl', price: 4.00, quantity: 0, type: 'cone' }
+  { name: 'Gummy Worms', price: 2.00, quantity: 0, type: 'topping' },
+  { name: 'Waffle Cone', price: 3.00, quantity: 0, type: 'cone' },
+  { name: 'Waffle Bowl', price: 4.00, quantity: 0, type: 'cone' },
   { name: 'Dipped Cone', price: 5.00, quantity: 0, type: 'cone' }
 ]
 
